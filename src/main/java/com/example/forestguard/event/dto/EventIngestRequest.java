16adee2f8e099e6b3de2ee822bffc1aeb9e49aa7
saraung <saraungbabu@gmail.com>
2026-01-27 21:52:07 +0530

    ty cleanup

--- src/main/java/com/example/forestguard/event/dto/EventIngestRequest.java
+++ src/main/java/com/example/forestguard/event/dto/EventIngestRequest.java
@@ -12,6 +12,5 @@ public class EventIngestRequest {
     private String label;
     private float confidence;
 
-    // true only when ESP32 prints "🚨 ALERT TRIGGERED"
     private boolean alertTriggered;
 }
